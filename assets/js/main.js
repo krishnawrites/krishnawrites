70fc488a9afc826dc28b0775417bebfbc4c1344d
krishna writes <61470785+krishnawrites@users.noreply.github.com>
2020-03-18 12:45:57 +0530

    files pushed

---
+++ assets/js/main.js
@@ -0,0 +1,88 @@
+(function () {
+  'use strict';
+
+  const stravaCardBody = document.querySelector('.strava-card .card-body');
+  let timeoutId;
+
+  showStartFetchingMessage();
+
+  getLatestActivity()
+    .then(response => {
+      clearTimeout(timeoutId);
+
+      const template = `
+        <h4>Latest activity</h4>
+        <div class="latest-run">
+          <a href="https://www.strava.com/activities/${ response.id }" target="_blank">
+            ${ response.name }
+          </a>
+          <div class="stats">
+            <div>
+              <span class="label">Distance</span>
+              <div>${ toKm(response.distance) } km</div>
+            </div>
+            <div>
+              <span class="label">Time</span>
+              <div>${ toMin(response.moving_time) } min</div>
+            </div>
+          </div>
+        </div>
+      `;
+
+      stravaCardBody.innerHTML = template;
+    })
+    .catch(() => {
+      stravaCardBody.innerHTML = '<p><small>Activity failed to load.</small></p>';
+    });
+
+  function toKm(distance) {
+    return (distance / 1000).toFixed(2);
+  }
+
+  function toMin(time) {
+    let hour = Math.floor(time / 3600);
+    let min = Math.floor((time - (hour * 3600)) / 60);
+    let sec = time - (hour * 3600) - (min * 60);
+
+    if (min.toString().length === 1) {
+      min = `0${min}`;
+    }
+    
+    if (sec.toString().length === 1) {
+      sec = `0${sec}`;
+    }
+
+    if (hour) {
+      return `${hour}:${min}:${sec}`;
+    }
+
+    return `${min}:${sec}`;
+  }
+      
+  function getLatestActivity() {
+    const url = "https://dzhstravaapp.azurewebsites.net/api/Activities";
+
+    timeoutId = setTimeout(() => {
+      stravaCardBody.innerHTML = getSlowRequestMessage();
+    }, 10 * 1000);
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(response => response[0]);
+  }
+
+  function getSlowRequestMessage() {
+    return `
+      <p>
+        <small>Oh, no! The request is running slow*...<br>
+        <strong>Not me, though! Wait and see!</strong> 🏃</small>
+      </p>
+      <p class="azure-function-message">
+        * It's actually an Azure Function trying to do a cold start.
+      </p>`
+  }
+
+  function showStartFetchingMessage() {
+    stravaCardBody.innerHTML = '<p><small>Getting latest activity...</small></p>';
+  }
+}());
---
+++ index.md
@@ -0,0 +1,14 @@
+---
+title: "krishna kakade"
+---
+
+### Latest posts
+
+<ul class="posts">
+  {% for post in site.posts %}
+    <li>
+      <div class="publish-date"><time pubdate="">{{ post.date | date: "%B %-d, %Y" }}</time></div>
+      <a href="{{ post.url }}">{{ post.title }}</a>
+    </li>
+  {% endfor %}
+</ul>
